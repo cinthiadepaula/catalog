c88285489c08589468863982a2a8153569a0c42b
cinthiadepaula <cinthiade.paula@outlook.com>
2022-09-06 00:06:48 -0300

    pull repositoy

---
+++ src/utilities/formatCurrency.ts
@@ -0,0 +1,10 @@
+const CURRENCY_FORMATTER = new Intl.NumberFormat(undefined, {
+    currency:"brl", style:"currency"
+})
+
+
+export function formatCurrency(number: number){
+    return(
+        CURRENCY_FORMATTER.format(number)
+    )
+}
